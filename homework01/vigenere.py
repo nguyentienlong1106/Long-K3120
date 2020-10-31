8a73b02b1723ff1e2fc981702f1390ef1b7872f2
nguyentienlong1106 <tienlong1106@gmail.com.vn>
2020-10-31 03:12:06 +0300

    Реализована функция encrypt_vigenere() и decrypt_vigenere()

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,3 +1,5 @@
+upper = {ascii:chr(ascii) for ascii in range(65,91)}
+lower = {ascii:chr(ascii) for ascii in range(97,123)}
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
@@ -11,6 +13,24 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     ciphertext = ""
     # PUT YOUR CODE HERE
+    arr=[]
+    for i in range(len(plaintext)):
+        keyword_c=keyword[i % len(keyword)]
+        if plaintext[i].isalpha():
+            if ord(plaintext[i]) in upper:
+                x=ord(plaintext[i])-65
+                y=ord(keyword_c)-65
+                z=chr((x+y)%26+65)
+            if ord(plaintext[i]) in lower:
+                x=ord(plaintext[i])-97
+                y=ord(keyword_c)-97
+                z=chr((x+y)%26+97)
+        else:
+            z=plaintext[i]
+        arr.append(z)
+    
+    ciphertext=ciphertext.join(arr)
+    print(ciphertext)
     return ciphertext
 
 
@@ -27,4 +47,26 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
     plaintext = ""
     # PUT YOUR CODE HERE
+    arr=[]
+    for i in range(len(ciphertext)):
+        keyword_c=keyword[i % len(keyword)]
+        if ciphertext[i].isalpha():
+            if ord(ciphertext[i]) in upper:
+                x=ord(ciphertext[i])-65
+                y=ord(keyword_c)-65
+                z=chr((x-y+26)%26+65)
+            if ord(ciphertext[i]) in lower:
+                x=ord(ciphertext[i])-97
+                y=ord(keyword_c)-97
+                z=chr((x-y+26)%26+97)
+        else:
+            z=ciphertext[i]
+        arr.append(z)
+    
+    plaintext=plaintext.join(arr)
+    print(plaintext)
     return plaintext
+
+encrypt_vigenere("introduction to python", "lsci")
+#, "tfvzzvwkeaqv lq aqvpzf"
+decrypt_vigenere("tfvzzvwkeaqv lq aqvpzf", "lsci")
